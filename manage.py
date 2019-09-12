d9d9317a3c8ac17b1b889353005ee51336b62460
zhangbo2008 <15122306087@163.com>
2019-09-12 22:06:57 +0800

    1

--- manage.py
+++ manage.py
@@ -2,13 +2,14 @@ from flask import Flask
 #flask更简单一个文件即可!
 app = Flask(__name__)
 from test import main
+import json
 import requests
 from flask import Flask,request
 @app.route('/',methods=['GET','POST'])
 
 def main1():
 
-    return str(main(request.form['pic']))
+    return json.dumps(main(request.form['pic']))
 
 
 
